9cc55fa3f0f03d74f44dc0be2e7c690cca2b0ff7
eomars <pakatsy@ciencias.unam.mx>
2016-03-04 00:14:50 -0600

    Ultimo commit del proyecto, sin validaciones

--- Ventas-RTec-ejb/src/java/com/rtec/Pintor.java
+++ Ventas-RTec-ejb/src/java/com/rtec/Pintor.java
@@ -8,12 +8,16 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.LinkedList;
 import javax.enterprise.context.ApplicationScoped;
 import javax.imageio.ImageIO;
 
 /**
  * Dibuja una gr&aacute;fica y la guarda en un PNG en el directorio <tt>img</tt>
  * del server.
+ *
  * @author <a href="mailto:manuel_castillo_cc@ciencias.unam.mx" >Manuel,
  * "Nachintoch", Castillo</a>
  * @version 1.0, february 2016
@@ -21,94 +25,244 @@ import javax.imageio.ImageIO;
  */
 @ApplicationScoped
 public class Pintor {
-    
+
     // métodos de implementación
-    
     /**
      * Pinta una gr&aacute;fica en un PNG llamado <tt>"grafica.png"</tt>.
+     *
      * @throws IOException - Si ocurre un problema al crear el archivo PNG.
      * @since Pintor 1.0, february 2016
      */
-    public void paintGraph() throws IOException {
+    public void paintGraph() throws IOException,SQLException, ClassNotFoundException {
+        ConexionBD con = new ConexionBD("rtec", "postgres", "root");
+        con.conectarBD();
+        ResultSet consultaBD = con.consultarBD("SELECT * FROM ventas order by fecha;");
+        con.desconectarBD();
+        LinkedList<Ventas> salida = new LinkedList<>();
+        while (consultaBD.next()) {
+            salida.add(new Ventas(consultaBD.getDouble("ventabruta"),
+                    consultaBD.getString("capturista"), consultaBD.getString("fecha")));
+        }
+        
+        paintGraph(salida.toArray(new Ventas[0]));
+        
+    
+    }
+
+    
+    
+    
+    /**
+     * Pinta una grafica en un PNG llamado <tt>"grafica.png"</tt>.
+     * @since Pintor 1.0, february 2016
+     */
+    public void paintGraph(Ventas[] ventas) {
         // creamos un buffer de imagen con dimenciones y propiedades dadas
-        BufferedImage graf =
-                new BufferedImage(1000, 500, BufferedImage.TYPE_4BYTE_ABGR);
-        // comprobamos si existe el directorio img
-        // creamos el archivo donde vamos aguardar la imagen
-        //la ruta es dependiendo de donde tengan alojado el proyecto, en mi caso 
-        //esta en dropbox     
-        File png = Paths.get("/", "home", "user", "Dropbox","riesgoTec","Practica1",
-                "Ventas-RTec", "Ventas-RTec-war", "build", "web", "img",
+        BufferedImage graf = new BufferedImage(1000, 700, BufferedImage.TYPE_4BYTE_ABGR);
+        File png = Paths.get("/", "home", "user","Dropbox","riesgoTec","Practica1","Ventas-RTEC",
+                "Ventas-RTec-war", "build", "web", "img",
                 "grafica.png").toFile();
         png.mkdirs();
+	try{
         png.createNewFile();
-        // recuperamos los gráficos para empezar a dibujar
+	}catch(Exception e){System.out.println("Error al crearArchivo");}
+        // recuperamos los grÃ¡ficos para empezar a dibujar
         Graphics2D g = graf.createGraphics();
         // asignamos un color para el dibujado
         g.setColor(Color.black);
         // dibujamos los ejes
-        g.drawLine(50, 450, 50, 50);
-        g.drawLine(50, 450, 750, 450);
+        g.drawLine(100, 650, 100, 50);//600pixeles de longitud vertical
+        g.drawLine(100, 650, 800, 650);//700 pixeles de longitud horizontal
+        g.setColor(Color.gray);
+        g.drawLine(100, 150, 800, 150);
+        g.setColor(Color.black);
         // asignamos una fuente para el texto
         g.setFont(new Font("Arial", Font.PLAIN, 25));
-        // dibujamos una cadena en la posición dada
-        g.drawString("Tiempo", 380, 480);
-        // respaldamos la configuración original del plano donde trabajamos
+        // dibujamos una cadena en la posiciÃ³n dada
+        g.drawString("TIEMPO", 380, 680);
+        // respaldamos la configuraciÃ³n original del plano donde trabajamos
         AffineTransform original = g.getTransform();
         // trasladamos el plano
-        g.translate(0, 280);
+        g.translate(0, 380);
         // rotamos 90 grados en sentido anti-horario.
         g.rotate(-Math.PI /2);
-        g.drawString("Ventas", 0, 25);
-        // restauramos la configuración original del plano
+        g.drawString("VENTAS", 0, 25);
+        // restauramos la configuraciÃ³n original del plano
         g.setTransform(original);
-        // dibujamos el perímetro de un rectángulo
-        g.drawRect(790, 100, 200, 100);
+        // dibujamos el perÃ­metro de un rectÃ¡ngulo
+        g.drawRect(800, 100, 200, 100);
         // cambiamos el color de dibujado
         g.setColor(Color.red);
-        // dibujamos el área de un rectángulo
-        g.fillRect(810, 110, 20, 20);
+        // dibujamos el Ã¡rea de un rectÃ¡ngulo
+        g.fillRect(820, 110, 20, 20);
         g.setColor(Color.green);
-        g.fillRect(810, 140, 20, 20);
+        g.fillRect(820, 140, 20, 20);
         g.setColor(Color.blue);
-        g.fillRect(810, 170, 20, 20);
+        g.fillRect(820, 170, 20, 20);
         g.setColor(Color.black);
         g.setFont(new Font("Arial", Font.PLAIN, 15));
-        g.drawString("Brutas", 850, 125);
-        g.drawString("Impuestos", 850, 155);
-        g.drawString("Netas", 850, 185);
+        g.drawString("Brutas", 860, 125);
+        g.drawString("Impuestos", 860, 155);
+        g.drawString("Netas", 860, 185);
         g.setColor(Color.red);
-        g.drawLine(50, 450, 100, 275);
-        g.drawLine(100, 275, 180, 250);
-        g.drawLine(180, 250, 260, 175);
-        g.drawLine(260, 175, 340, 325);
-        g.drawLine(340, 325, 420, 300);
-        g.drawLine(420, 300, 500, 350);
-        g.drawLine(500, 350, 580, 275);
-        g.drawLine(580, 275, 660, 250);
-        g.drawLine(660, 250, 740, 200);
-        g.setColor(Color.green);
-        g.drawLine(50, 450, 100, 400);
-        g.drawLine(100, 400, 180, 375);
-        g.drawLine(180, 375, 260, 350);
-        g.drawLine(260, 350, 340, 425);
-        g.drawLine(340, 425, 420, 400);
-        g.drawLine(420, 400, 500, 425);
-        g.drawLine(500, 425, 580, 400);
-        g.drawLine(580, 400, 660, 375);
-        g.drawLine(660, 375, 740, 350);
+        //genero variables para usarlas al pintar cada grafica
+        int xAnt = 100;
+        int yAnt = 650;
+        int xSig = 0;
+        int ySig = 0;
+        //divido el total de fechas entre los pixeles a usar
+        int xr3 = (int)(700/ventas.length);
+        double yr3 = factorEscala(ventas,1,g);
+        g.setColor(Color.red);
+        pintaGrafica(g,ventas,1,xAnt,yAnt,xSig,ySig,xr3,yr3);
+        //reinicio variables al origen de la grafica
+        xAnt = 100;
+        yAnt = 650;
+        xSig = 0;
+        ySig = 0;
+        xr3 = (int)(700/ventas.length);
+        yr3 = factorEscala(ventas,2,g);
+        // Se define el componente B.
         g.setColor(Color.blue);
-        g.drawLine(50, 450, 100, 325);
-        g.drawLine(100, 325, 180, 300);
-        g.drawLine(180, 300, 260, 250);
-        g.drawLine(260, 250, 340, 375);
-        g.drawLine(340, 375, 420, 350);
-        g.drawLine(420, 350, 500, 375);
-        g.drawLine(500, 375, 580, 325);
-        g.drawLine(580, 325, 660, 300);
-        g.drawLine(660, 300, 740, 250);
+        pintaGrafica(g,ventas,2,xAnt,yAnt,xSig,ySig,xr3,yr3);
+        //reinicio variables al origen de la grafica
+        xAnt = 100;
+        yAnt = 650;
+        xSig = 0;
+        ySig = 0;
+        xr3 = (int)(700/ventas.length);
+        yr3 = factorEscala(ventas,3,g);
+        // Se define el componente G
+        g.setColor(Color.green);
+	pintaGrafica(g,ventas,3,xAnt,yAnt,xSig,ySig,xr3,yr3);
         g.dispose();
-        ImageIO.write(graf, "PNG", png);
+        try{ImageIO.write(graf, "PNG", png);}
+        catch(Exception e){System.out.println("Error al pintar.");}
     }//paintGraph
-    
+
+    /**
+     * Metodo que dibuja la gráfica específica de un tipo de dato en un conjuntos de ventas.
+     * @param g Es la grafica en la que se dibuja.
+     * @param ventas Es el conjunto de ventas de donde se obtiene la información.
+     * @param dato Tipo de Venta de la que se genera la gráfica.
+     * @param xAnt Es la coordenada X en la gráfica de la recta anterior dibujada.
+     * @param yAnt Es la coordenada Y en la gráfica de la recta anterior dibujada.
+     * @param xSig Es la coordenada X en la gráfica de la recta siguiene a dibujar.
+     * @param ySig Es la coordenada Y en la gráfica de la recta siguiene a dibujar.
+     * @param xr3 Es la coordenada X del espacio donde comienza lla recta a graficar.
+     * @param yr3 Es la coordenada Y del espacio donde comienza lla recta a graficar.
+     */
+    public void pintaGrafica(Graphics2D g,Ventas[] ventas,int dato, int xAnt,int yAnt, int xSig,int ySig,int xr3,double yr3){
+	if(dato == 1){
+	    double minimo = minimoVentas(ventas);
+	    double temp;
+	    for(int i=0; i<ventas.length;i++){
+		xSig = xAnt + xr3;
+		temp = yr3*ventas[i].getVentaBruta();
+		ySig = 650 - (int)temp;
+		if(minimo == ventas[i].getVentaBruta()){
+		    g.setColor(Color.gray);
+		    g.drawLine(100, ySig, 800, ySig);
+		    g.setFont(new Font("comic Sans", Font.PLAIN, 15));
+		    g.setColor(Color.black);
+		    g.drawString("$" + minimo, 3, ySig);
+		    g.setColor(Color.red);
+		}
+
+		g.fillOval(xSig-2,ySig-2,5,5);
+		g.drawLine(xAnt,yAnt,xSig,ySig);
+		xAnt = xSig;
+		yAnt = ySig;
+	    }
+	}
+	if(dato == 2){
+	    for(int i=0; i<ventas.length;i++){
+		xSig = xAnt + xr3;
+		double temp = yr3*ventas[i].getVentaNeta();
+		ySig = 650 - (int)temp;
+		
+		g.fillOval(xSig-2,ySig-2,5,5);
+		g.drawLine(xAnt,yAnt,xSig,ySig);
+		xAnt = xSig;
+		yAnt = ySig;
+	    }	
+	}
+	if(dato == 3){
+	    for(int i=0; i<ventas.length;i++){
+		xSig = xAnt + xr3;
+		double temp = yr3*ventas[i].getIva();
+		ySig = 650 - (int)temp;
+		
+		g.fillOval(xSig-2,ySig-2,5,5);
+		g.drawLine(xAnt,yAnt,xSig,ySig);
+		xAnt = xSig;
+		yAnt = ySig;
+	    }	
+	}
+
+    }
+
+    /**
+     * Método que calcula la distancia entre los puntos de venta.
+     * @param numVentas Es el total de ventas.
+     * @return Devuelve la distancia entre las ventas.
+     */
+    public int distanciaX(int numVentas){
+	return 700/numVentas;
+    }
+
+    /**
+     * Calcula el valor mínimo de las ventas.
+     * @param ventas Es el conjunto de Ventas.
+     * @return Devuelve el valor entero de la venta mínima.
+     */
+    public double minimoVentas(Ventas[] ventas){
+	double minimo = ventas[0].getVentaBruta();
+	for(int i = 0; i<ventas.length;i++){
+	    if(minimo > ventas[i].getVentaBruta()){
+		minimo = ventas[i].getVentaBruta();
+	    }
+	}
+	return minimo;
+    }
+
+    /**
+     * Metodo que cualcula el factor de escala para la altura en pixeles de las ventas en la gráfica.
+     * @param ventas Es el conjunto de ventas.
+     * @param x Es el tipo de dato de venta.
+     * @param g Es la gráfica en la que se dibjua.
+     * @return Devuelve el factor de escala para graficar las ventas.
+     */
+    public double factorEscala(Ventas[] ventas,int x,Graphics2D g){
+	double maximo = -1;
+	for(int i = 0; i<ventas.length;i++){
+	    if(x == 1){
+		if(ventas[i].getVentaBruta() > maximo){
+		    maximo = ventas[i].getVentaBruta();
+		    
+		}
+	    }
+	    if(x == 2){
+		if(ventas[i].getVentaNeta() > maximo){
+		    maximo = ventas[i].getVentaBruta();
+		}
+	    }
+	    if(x == 3){
+		if(ventas[i].getIva() > maximo){
+		    maximo = ventas[i].getVentaBruta();
+		}
+	    }
+
+	}
+	if(x == 1){
+	    g.setFont(new Font("comic Sans", Font.PLAIN, 15));
+	    g.setColor(Color.black);
+	    g.drawString("$" + maximo, 3, 150);
+	}
+	maximo = 500/maximo;
+
+
+	return maximo;//el numero mas grande entre 500 pixeles a repartir
+    }    
+
 }//clase Pintor
--- base.sql
+++ base.sql
@@ -4,4 +4,6 @@ capturista varchar(64) NOT NULL,
 ventabruta real
 );
 
+alter table ventas alter column ventabruta type  double precision;
+
 INSERT INTO ventas(fecha,capturista,ventabruta) VALUES('2016-03-01','OMAR',12000.05);
